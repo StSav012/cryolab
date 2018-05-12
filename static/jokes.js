5efa834638f3b2a2f91ce229d08c193e68a55ab6
StSav012 <stsav012@gmail.com>
2018-05-12 18:59:16 +0300

    UI changes
    Replaced browser alerts with eye-candy divs.
    The turning on/off button becomes disabled when pressed to avoid double clicks.

--- static/jokes.js
+++ static/jokes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var jokes_perpage = 1;
 var jokes_type = "category";
 var url = 'jokes';
@@ -12,7 +14,8 @@ function loadJoke(selector){
         success: function(response){
             var jokes = JSON.parse(response).jokes;
             if (jokes.length > 0) {
-	            $(selector).first().text(jokes[0].joke_text);
+	        $(selector).first().text(jokes[0].joke_text);
+                window.scrollTo(0, document.body.scrollHeight);
             }
             else {
                 $(selector).first().text("");
@@ -20,4 +23,35 @@ function loadJoke(selector){
         }
     });
 }
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; ++i) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$(function() {
+    var d = new Date()
+    if (d.getDate() == 26 && d.getMonth() == 0 && getCookie("joke_of_the_day") != 1) {
+        $(document.body).css({transform: "rotate(180deg)"});
+        setCookie("joke_of_the_day", 1, 1);
+    }
+});
 
